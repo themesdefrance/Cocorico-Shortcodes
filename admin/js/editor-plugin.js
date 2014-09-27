f0e7a5ccd8d90100678ee960837a4ea039e78e39
Alex Borto <alex@themesdefrance.fr>
2014-09-27 20:22:10 +0200

    Message box shortcodes

--- admin/js/editor-plugin.js
+++ admin/js/editor-plugin.js
@@ -13,68 +13,130 @@
             style: buttonstyle,
             tooltip: 'Cocorico Shortcodes',
             menu: [
-            		
-            		{	text: editor.getLang('coco_shortcodes_tinymce.column'), 
-            		
+            		// BEGIN column
+            		{	text: editor.getLang('coco_shortcodes_tinymce.column'),
+            			
+            			// BEGIN onclick
             			onclick: function() {
-				            // Open window
+            			
+				            // BEGIN Popup
 				            editor.windowManager.open({
 				                title: editor.getLang('coco_shortcodes_tinymce.column_add'),
-				                body: [
-				                	{	type: 'label',
-				                		text: editor.getLang('coco_shortcodes_tinymce.column_howto')
-				                	},
-				                										
-				                    {	type: 'listbox',
-				                    	name: 'size_col',
-				                    	label: editor.getLang('coco_shortcodes_tinymce.column_size'),
-				                    	values:[	{text: '1/2', value: editor.getLang('coco_shortcodes_tinymce.column_one_half')},
-							            			{text: '1/3', value: editor.getLang('coco_shortcodes_tinymce.column_one_third')},
-							            			{text: '2/3', value: editor.getLang('coco_shortcodes_tinymce.column_two_thirds')},
-							            			{text: '1/4', value: editor.getLang('coco_shortcodes_tinymce.column_one_fourth')},
-							            			{text: '3/4', value: editor.getLang('coco_shortcodes_tinymce.column_three_fourths')}
-											    ]
-				                    },
-				                    
-				                    {	type: 'listbox',
-				                    	name: 'position_col',
-				                    	label: editor.getLang('coco_shortcodes_tinymce.column_position'),
-				                    	values:[	{	text: editor.getLang('coco_shortcodes_tinymce.column_first'),
-				                    					value: editor.getLang('coco_shortcodes_tinymce.column_first_value')
-				                    				},
-				                    				{	text: editor.getLang('coco_shortcodes_tinymce.column_middle'),
-				                    					value: editor.getLang('coco_shortcodes_tinymce.column_middle_value')
-				                    				},
-							            			{	text: editor.getLang('coco_shortcodes_tinymce.column_last'),
-							            				value: editor.getLang('coco_shortcodes_tinymce.column_last_value')
-							            			}
-											    ]
-				                    },
-				                    
-				                    {	type: 'textbox',
-				                    	name: 'contenu_col',
-				                    	label: editor.getLang('coco_shortcodes_tinymce.column_content'),
-				                    	multiline: true,
-				                    	minWidth: 300,
-				                    	minHeight: 100
-									},
+				                // BEGIN body popup
+				                body: [	{	type: 'listbox',
+					                    	name: 'size_col',
+					                    	label: editor.getLang('coco_shortcodes_tinymce.column_size'),
+					                    	values:[	{text: '1/2', value: editor.getLang('coco_shortcodes_tinymce.column_one_half')},
+								            			{text: '1/3', value: editor.getLang('coco_shortcodes_tinymce.column_one_third')},
+								            			{text: '2/3', value: editor.getLang('coco_shortcodes_tinymce.column_two_thirds')},
+								            			{text: '1/4', value: editor.getLang('coco_shortcodes_tinymce.column_one_fourth')},
+								            			{text: '3/4', value: editor.getLang('coco_shortcodes_tinymce.column_three_fourths')}
+												    ]
+					                    },
+					                    
+					                    {	type: 'listbox',
+					                    	name: 'position_col',
+					                    	label: editor.getLang('coco_shortcodes_tinymce.column_position'),
+					                    	values:[	{	text: editor.getLang('coco_shortcodes_tinymce.column_first'),
+					                    					value: editor.getLang('coco_shortcodes_tinymce.column_first_value')
+					                    				},
+					                    				{	text: editor.getLang('coco_shortcodes_tinymce.column_middle'),
+					                    					value: editor.getLang('coco_shortcodes_tinymce.column_middle_value')
+					                    				},
+								            			{	text: editor.getLang('coco_shortcodes_tinymce.column_last'),
+								            				value: editor.getLang('coco_shortcodes_tinymce.column_last_value')
+								            			}
+												    ]
+					                    },
+					                    
+					                    {	type: 'textbox',
+					                    	name: 'content_col',
+					                    	label: editor.getLang('coco_shortcodes_tinymce.column_content'),
+					                    	multiline: true,
+					                    	minWidth: 300,
+					                    	minHeight: 100
+										}
 
 				                ],
+				                // END body popup
+				                
 				                onsubmit: function(e) {
 				                
 				                	var res = '';
 				                	
 				                	res += '[' + editor.getLang('coco_shortcodes_tinymce.column_shortcode') + ' ' + editor.getLang('coco_shortcodes_tinymce.column_size_att') + '="' + e.data.size_col + '" ' + editor.getLang('coco_shortcodes_tinymce.column_position_att') + '="' + e.data.position_col+ '"]<br>';
-				                	res += e.data.contenu_col + '<br>[/' + editor.getLang('coco_shortcodes_tinymce.column_shortcode') + ']<br><br>';
+				                	res += e.data.content_col + '<br>[/' + editor.getLang('coco_shortcodes_tinymce.column_shortcode') + ']<br><br>';
 				                	
 				                    editor.insertContent(res);
 				                }
 				                
 				                
 				            });
+				            // END Popup
 				        },
+            			// END onclick
+            		},
+					// END column
+					            		
+            		
+            		// BEGIN message
+            		{	text: editor.getLang('coco_shortcodes_tinymce.message'),
             			
-            		}
+            			// BEGIN onclick
+            			onclick: function() {
+            			
+				            // BEGIN Popup
+				            editor.windowManager.open({
+				                title: editor.getLang('coco_shortcodes_tinymce.message_add'),
+				                // BEGIN body popup
+				                body: [	{	type: 'listbox',
+					                    	name: 'message_type',
+					                    	label: editor.getLang('coco_shortcodes_tinymce.message_type'),
+					                    	values:[	{	text: editor.getLang('coco_shortcodes_tinymce.message_info'),
+					                    					value: editor.getLang('coco_shortcodes_tinymce.message_info_value')
+					                    				},
+					                    				{	text: editor.getLang('coco_shortcodes_tinymce.message_alert'),
+					                    					value: editor.getLang('coco_shortcodes_tinymce.message_alert_value')
+					                    				},
+								            			{	text: editor.getLang('coco_shortcodes_tinymce.message_error'),
+								            				value: editor.getLang('coco_shortcodes_tinymce.message_error_value')
+								            			},
+								            			{	text: editor.getLang('coco_shortcodes_tinymce.message_success'),
+								            				value: editor.getLang('coco_shortcodes_tinymce.message_success_value')
+								            			}
+												    ]
+										},
+				                    
+					                    {	type: 'textbox',
+					                    	name: 'message_content',
+					                    	label: editor.getLang('coco_shortcodes_tinymce.message_content'),
+					                    	multiline: true,
+					                    	minWidth: 300,
+					                    	minHeight: 100
+										}
+
+				                ],
+				                // END body popup
+				                
+				                onsubmit: function(e) {
+				                
+				                	var res = '';
+				                	
+				                	res += '[' + editor.getLang('coco_shortcodes_tinymce.message_shortcode') + ' ' + editor.getLang('coco_shortcodes_tinymce.message_type_att') + '="' + e.data.message_type + '"]<br>';
+				                	res += e.data.message_content + '<br>[/' + editor.getLang('coco_shortcodes_tinymce.message_shortcode') + ']<br><br>';
+				                	
+				                    editor.insertContent(res);
+				                }
+				                
+				                
+				            });
+				            // END Popup
+				        },
+            			// END onclick
+            		},
+					// END column
+            		
+            		
             		
 	        ]
             
--- cocorico-shortcodes-library.php
+++ cocorico-shortcodes-library.php
@@ -7,29 +7,29 @@ if(!function_exists('coco_shortcodes_column')){
 	function coco_shortcodes_column($atts, $content = null) {
 		
 		extract(shortcode_atts(array(
-			__('size', 'cocoshortcodes') => '',
-			__('position', 'cocoshortcodes') => ''
+			_x('size', 'shortcode attribute name', 'cocoshortcodes') => '',
+			_x('position', 'shortcode attribute name', 'cocoshortcodes') => ''
 		), $atts));
 		
-		$size = ${__('size', 'cocoshortcodes')};
-		$position = ${__('position', 'cocoshortcodes')};
+		$size = ${_x('size', 'shortcode attribute name', 'cocoshortcodes')};
+		$position = ${_x('position', 'shortcode attribute name', 'cocoshortcodes')};
 		
 		$classes = 'cs_column ';
 		
 		switch($size){
-			case __('one_half', 'cocoshortcodes') :
+			case _x('one_half', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= 'cs_one_half';
 			break;
-			case __('one_third', 'cocoshortcodes') :
+			case _x('one_third', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= 'cs_one_third';
 			break;
-			case __('two_thirds', 'cocoshortcodes') :
+			case _x('two_thirds', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= 'cs_two_thirds';
 			break;
-			case __('one_fourth', 'cocoshortcodes') :
+			case _x('one_fourth', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= 'cs_one_fourth';
 			break;
-			case __('three_fourths', 'cocoshortcodes') :
+			case _x('three_fourths', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= 'cs_three_fourths';
 			break;
 			default :
@@ -37,13 +37,13 @@ if(!function_exists('coco_shortcodes_column')){
 		}
 		
 		switch($position){
-			case __('first', 'cocoshortcodes') :
+			case _x('first', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= ' cs_column_first';
 			break;
-			case __('middle', 'cocoshortcodes') :
+			case _x('middle', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= ' cs_column_middle';
 			break;
-			case __('last', 'cocoshortcodes') :
+			case _x('last', 'shortcode attribute value', 'cocoshortcodes') :
 				$classes .= ' cs_column_last';
 			break;
 			default:
@@ -54,13 +54,52 @@ if(!function_exists('coco_shortcodes_column')){
 		$res .= do_shortcode(wpautop($content));
 		$res .= '</div>';
 		
-		$res .= ($position == __('last', 'cocoshortcodes') ? '<div class="cs_clear"></div>' : '');
+		$res .= ($position == _x('last', 'shortcode attribute name', 'cocoshortcodes') ? '<div class="cs_clear"></div>' : '');
 		
 		return $res;
 	}
 }
 
+// Shortcode Message generator
+if (!function_exists('coco_shortcodes_message')){
+	function coco_shortcodes_message($atts, $content = null){
+		
+		extract(shortcode_atts(array(
+			_x('type', 'shortcode attribute name', 'cocoshortcodes') => '',
+		), $atts));
+		
+		$type = ${_x('type', 'shortcode attribute name', 'cocoshortcodes')};
+		
+		$classes = 'cs_message ';
+		
+		switch($type){
+			case _x('info', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_info';
+			break;
+			case _x('alert', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_alert';
+			break;
+			case _x('error', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_error';
+			break;
+			case _x('success', 'shortcode attribute value', 'cocoshortcodes') :
+				$classes .= 'cs_success';
+			break;
+			default :
+			 	$classes .= '';
+		}
+		
+		$res = '<div class="'.$classes.'">';
+		$res .= do_shortcode(wpautop($content));
+		$res .= '</div>';
+		return $res;
+	}
+}
+
 function coco_shortcodes_register(){
-   add_shortcode(__('cocorico_column', 'cocoshortcodes'), 'coco_shortcodes_column');
+   add_shortcode(_x('cocorico_column', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_column');
+   add_shortcode(_x('cocorico_message', 'shortcode name', 'cocoshortcodes'), 'coco_shortcodes_message');
 }
 add_action( 'init', 'coco_shortcodes_register');
+
+
